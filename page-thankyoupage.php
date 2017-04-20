aad71aaa6e69417c6c9499e917af43a5bca9bade
Annabelle Casalhay <annabelle@purplepatchservices.com>
2017-04-20 14:28:23 +0800

    Include Tracking code in Thank you Page

--- page-thankyoupage.php
+++ page-thankyoupage.php
@@ -93,6 +93,40 @@ foreach ($_SERVER as $key => $value) {
 </div>
 
 <!--</div> #main -->
+
+<!-- Google Code for Bedrock Analytics Website Complete Conversion Page -->
+
+<!-- Google Adwords -->
+<script type="text/javascript">
+    /* <![CDATA[ */
+    var google_conversion_id = 854853502;
+    var google_conversion_language = "en";
+    var google_conversion_format = "3";
+    var google_conversion_color = "ffffff";
+    var google_conversion_label = "7XuSCKHwwHAQ_o7QlwM";
+    var google_remarketing_only = false;
+    /* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+    <div style="display:inline;">
+        <img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/854853502/?label=7XuSCKHwwHAQ_o7QlwM&amp;guid=ON&amp;script=0"/>
+    </div>
+</noscript>
+
+<!-- LinkedIn Conversion -->
+<script type="text/javascript">
+    _linkedin_data_partner_id = "40305";
+</script>
+<script type="text/javascript">
+    (function(){var s = document.getElementsByTagName("script")[0];
+    var b = document.createElement("script");
+    b.type = "text/javascript";b.async = true;
+    b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+    s.parentNode.insertBefore(b, s);})();
+</script>
+
 <?php wp_footer(); ?>
 </body>
 </html>
